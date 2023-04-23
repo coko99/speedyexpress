e8b54c4ecb3a475da9a5976b255353403f1501d6
miljkomocnik <miljkomocnik@gmail.com>
2023-04-23 19:14:28 +0200

    Fix for database name

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
    define('DB_SERVER', 'localhost:8889');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'root');
-   define('DB_DATABASE', 'prochef');
+   define('DB_DATABASE', 'speedyex_database');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    $db->set_charset("utf8");
 ?>
